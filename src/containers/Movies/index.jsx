829ac6a082bd28fcbdd82f05be46aa7861bfe48c
feliperyo <ryofeliperyo@gmail.com>
2024-03-19 11:52:04 -0300

    primeiro commit

---
+++ src/containers/Movies/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Movies() {
+  return (
+    <>
+      <h1>Filmes</h1>
+      <p>Essa é a Filmes</p>
+    </>
+  );
+}
+
+export default Movies
---
+++ src/containers/Series/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Series() {
+  return (
+    <>
+      <h1>Series</h1>
+      <p>Essa é a Series</p>
+    </>
+  );
+}
+
+export default Series
