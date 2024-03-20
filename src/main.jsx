02778a1acc2b85a5515025f5d7ad94a08b9f030b
feliperyo <ryofeliperyo@gmail.com>
2024-03-20 08:54:49 -0300

    formatando

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import GlobalStyle from "styled-components"
-import { BrowserRouter } from "react-router-dom"
-import Router from "./routes/routes.jsx"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import GlobalStyle from 'styled-components'
+
+import Router from './routes/routes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
     <GlobalStyle />
   </React.StrictMode>
-);
+)
