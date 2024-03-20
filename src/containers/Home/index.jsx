d105040c7afbf8dc00d8ad14569f7130db4b9f9c
feliperyo <ryofeliperyo@gmail.com>
2024-03-20 08:43:38 -0300

    adicionando API

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,12 +1,19 @@
-import React from "react"
+import api from '../../services/api'
 
 function Home() {
+  async function getMovies() {
+    const data = await api.get('/movie/popular')
+    console.log(data)
+  }
+
+  getMovies()
+
   return (
     <>
       <h1>Home</h1>
       <p>Essa é a Home</p>
     </>
-  );
+  )
 }
 
 export default Home
