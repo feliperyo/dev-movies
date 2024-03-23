0be18906542a1104d3f43b89985072ced738d3d7
feliperyo <ryofeliperyo@gmail.com>
2024-03-23 13:17:25 -0300

    adicionando rota Detail

--- src/containers/Detail/styles.js
+++ src/containers/Detail/styles.js
@@ -1,7 +1,112 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
+const scale = keyframes`
+from{
+  transform: scale(0);
+}
+to{
+  transform: scale(1);
+}
+`;
+
+export const Background = styled.div`
+  background-image: url(${(props) => props.image});
+  height: 50vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 120px;
+    background-image: linear-gradient(to top, #000, rgba(0, 0, 0, 0));
+  }
+`;
+
+export const Cover = styled.div`
+  padding: 20px;
+  display: flex;
+  align-items: flex-start;
+  height: 100%;
+  z-index: 99;
+
+  img {
+    width: 400px;
+    border-radius: 30px;
+    box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
+    animation: ${scale} 0.5s linear;
+  }
+`;
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100%;
+  max-width: 1500px;
+  margin-top: -100px;
+`;
 
+export const Info = styled.div`
+  z-index: 99;
+  color: #ffffff;
+  padding: 20px;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
 
-`
+  h2 {
+    font-size: 50px;
+    font-weight: 700;
+  }
+
+  p {
+    font-weight: 700;
+  }
+`;
+
+export const ContainerMovies = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  width: 100%;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    max-width: 1000px;
+    width: 100%;
+    height: 100%;
+    margin: 20px 0;
+  }
+
+  h4 {
+    font-size: 20px;
+    font-weight: 700;
+    color: #ffffff;
+    margin-bottom: 10px;
+  }
+
+  iframe {
+    border: none;
+  }
+`;
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -1,64 +1,84 @@
-import styled from 'styled-components'
+import styled, { keyframes } from "styled-components";
+
+const scale = keyframes`
+from{
+    transform: scale(0);
+} to{
+    transform: scale(1);
+}
+`;
 
 export const Background = styled.div`
-background-image: url(${props => props.img});
-height: 100vh;
-background-position: center;
-background-size:cover;
-display: flex;
-align-items: center;
-justify-content: center;
+  background-image: url(${(props) => props.img});
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
 
-&::before{
-content: '';
-position: absolute;
-top: 0;
-left: 0;
-width: 100%;
-height: 100vh;
-background-color: rgba(0,0,0,0.5);
-}
-`
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  &::after {
+    content: "";
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    height: 120px;
+    background-image: linear-gradient(to top, #000, rgba(0, 0, 0, 0));
+  }
+`;
 
 export const Container = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-around;
-height: 100%;
-max-width: 1500px;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  height: 100%;
+  max-width: 1500px;
+`;
 
 export const Info = styled.div`
-z-index: 2;
-padding: 20px;
-width: 50%;
+  z-index: 2;
+  padding: 20px;
+  width: 50%;
 
-h1{
+  h1 {
     font-size: 60px;
     font-weight: 700;
     color: #ffffff;
-}
+  }
 
-p{
+  p {
     font-size: 20px;
-    font-weight:500;
+    font-weight: 500;
     color: #ffffff;
     margin-top: 30px;
     margin-bottom: 20px;
-}
-`
+  }
+`;
 export const Poster = styled.div`
-z-index: 2;
+  z-index: 2;
 
-img{
+  img {
     width: 400px;
     border-radius: 30px;
-}
-`
+    animation: ${scale} 0.5s linear;
+    box-shadow: rgb(0 0 0 / 75%) 0px 7px 29px 0px;
+  }
+`;
 
 export const ContainerButtons = styled.div`
-display: flex;
-gap: 20px;
-margin-top: 30px;
-`
+  display: flex;
+  gap: 20px;
+  margin-top: 30px;
+`;
